74420c071d66b4921ac6743233b85332ac82f46f
Mateus Rossatto <mateusrossatto@gmail.com>
2020-01-17 23:20:24 -0300

    add main function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,8 +1,14 @@
 defmodule Identicon do
+  
+  def main(input) do
+    hash_input(input)
+  end
+
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
   end
+
 end
